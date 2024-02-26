3d1a07308ff28717012a87bac9cc648141efe381
amaz11 <k.m.shaonred@gmail.com>
2024-02-26 11:58:46 +0600

    cart oparetion done

--- src/components/Card/ProductCard.jsx
+++ src/components/Card/ProductCard.jsx
@@ -1,16 +1,23 @@
-import React from 'react'
+import React, { useContext } from 'react'
+
+// Context api
+import { CartContextCreate } from '../../hoc/cartContext'
 
 // image
 import errorImage from "../../assets/504708-200.png"
 
 const ProductCard = ({ item }) => {
+    const { addToCart } = useContext(CartContextCreate)
     const imageOnError = (event) => {
         event.currentTarget.src = errorImage;
     }
     return (
-        <div className='border rounded'>
-            <div className=''>
+        <div className='border rounded group'>
+            <div className='relative'>
                 <img className='w-full rounded-t' src={item.images[1]} alt="img" onError={imageOnError} />
+                <button className='group-hover:block hidden absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-[#E7E9F6] text-[#5C6AC4] z-10 text-xs font-medium rounded py-2 px-2' onClick={() => addToCart(item)}>Add To Cart</button>
+                <div className='group-hover:block hidden absolute top-0 left-0 h-full w-full bg-slate-300 opacity-50'>
+                </div>
             </div>
             <div className='text-center'>
                 <p className='text-slate-600 text-lg font-semibold border-b p-1'>${item.price}</p>
---
+++ src/hoc/cartContext.jsx
@@ -0,0 +1,72 @@
+import React, { createContext, useEffect, useState } from 'react'
+
+export const CartContextCreate = createContext(null)
+
+const CartContextProvider = ({ children }) => {
+    const [cart, setCart] = useState([])
+    const ls = typeof window !== 'undefined' ? window.localStorage : null
+
+    const addToCart = (value) => {
+        const newCart = [...cart]
+        const findCartProduct = newCart.find(c => c.id === value.id)
+        if (findCartProduct) {
+            findCartProduct.quantity += 1;
+            setCart(newCart)
+            return ls?.setItem('cart', JSON.stringify(cart))
+
+        }
+        else {
+            const newValue = { ...value, quantity: 1 }
+            const newArr = [...cart, newValue]
+            setCart(newArr)
+            return ls?.setItem('cart', JSON.stringify(newArr))
+        }
+
+    }
+    const removeFromCart = (id) => {
+        const newCart = [...cart]
+        const filttercart = newCart.filter((value) => value.id !== id)
+        setCart(filttercart)
+        ls?.setItem('cart', JSON.stringify(filttercart))
+
+    }
+
+    const increseQuantity = (value) => {
+        const newCart = [...cart]
+        const findCartProduct = newCart.find(c => c.id === value)
+        if (findCartProduct) {
+            findCartProduct.quantity += 1;
+            setCart(newCart)
+            return ls?.setItem('cart', JSON.stringify(cart))
+        }
+        return
+    }
+
+    const deincreseQuantity = (value) => {
+        const newCart = [...cart]
+        const findCartProduct = newCart.find(c => c.id === value)
+        if (findCartProduct) {
+            findCartProduct.quantity -= 1;
+            setCart(newCart)
+            return ls?.setItem('cart', JSON.stringify(cart))
+
+        }
+        return
+    }
+
+
+    useEffect(() => {
+        if (ls && ls.getItem('cart')) {
+            const localCart = JSON.parse(ls.getItem('cart'))
+            setCart(localCart);
+        }
+    }, []);
+
+    return (
+        <CartContextCreate.Provider value={{ cart, addToCart, removeFromCart, increseQuantity, deincreseQuantity }}>
+            {children}
+        </CartContextCreate.Provider>
+    )
+}
+
+export default CartContextProvider
