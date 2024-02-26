c1602b6fa0bdd03a8031674ad84d5f4a738abe39
amaz11 <k.m.shaonred@gmail.com>
2024-02-26 19:08:03 +0600

    check done

--- src/components/ButtonGroup/ButtonGroup.jsx
+++ src/components/ButtonGroup/ButtonGroup.jsx
@@ -9,7 +9,7 @@ import { FaAmazonPay } from "react-icons/fa";
 // Context API
 import { CartContextCreate } from '../../hoc/cartContext';
 const ButtonGroup = () => {
-    const { pay, setPay } = useContext(CartContextCreate)
+    const { pay, setPay, cart } = useContext(CartContextCreate)
     return (
         <div className='mb-3 mt-4 flex items-center justify-between flex-wrap gap-4'>
             <button className='flex gap-3 cursor-pointer items-center justify-center bg-red-200 py-3 px-4 rounded text-red-600 grow'>
@@ -27,7 +27,7 @@ const ButtonGroup = () => {
                 <span className='font-semibold text-xl'>Discount</span>
             </button>
 
-            <button onClick={() => setPay(!pay)} className='flex gap-3 cursor-pointer items-center justify-center py-3 px-4 rounded bg-[#E7E9F6] text-[#5C6AC4] grow'>
+            <button onClick={() => setPay(!pay)} className='flex gap-3 cursor-pointer items-center justify-center py-3 px-4 rounded bg-[#E7E9F6] text-[#5C6AC4] grow' disabled={cart.length === 0 ? true : false}>
                 <FaAmazonPay size={28} />
                 <span className='font-semibold text-xl' >Pay Now</span>
             </button>
--- src/components/Card/ProductCard.jsx
+++ src/components/Card/ProductCard.jsx
@@ -15,7 +15,7 @@ const ProductCard = ({ item }) => {
     return (
         <div className='border rounded group'>
             <div className='relative'>
-                <img className='w-full rounded-t' src={item.images[1]} onError={imageOnError} loading='lazy' alt="img" />
+                <img className='w-full rounded-t' src={item.category.image} onError={imageOnError} loading='lazy' alt="img" />
                 <button className='group-hover:block hidden absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-[#E7E9F6] text-[#5C6AC4] z-10 text-xs font-medium rounded py-2 px-2' onClick={() => addToCart(item)} >Add To Cart</button>
                 <div className='group-hover:block hidden absolute top-0 left-0 h-full w-full bg-slate-300 opacity-50'>
                 </div>
